e35ece3f1f5fcfd6bcc571d37b17a1134ebebb15
mshaveta <shaveta.malhotra47@gmail.com>
2021-09-13 09:33:31 +0530

    Added

---
+++ LearnJava/PrintVals.java
@@ -0,0 +1,12 @@
+class PrintVals{
+	public static void main(String[] args){
+		int a=10; 
+		 
+		 
+		System.out.println(a);//var value
+		System.out.println("a");
+		System.out.println("abvcsfsfd");
+		 
+		
+	}
+}
---
+++ LearnJava/Variable.java
@@ -0,0 +1,26 @@
+class Variable{
+	public static void main(String[] args){
+		int a;//var. decl.
+		a=100;//init. / value assignment
+		int x = 200;// decl. & init.
+		
+		int b,c,d;
+		b=20;
+		c=30;
+		d=40;
+		
+		int y=100,z=200,m=300;
+		 
+		System.out.println(a);
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(m);
+		
+	}
+}
