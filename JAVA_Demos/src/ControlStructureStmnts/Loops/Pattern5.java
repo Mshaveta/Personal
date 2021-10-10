52cf55db8a852fc1bbc705d163b331c471621b28
Mshaveta <shaveta.malhotra47@gmail.com>
2021-10-10 20:42:12 +0530

    Done

---
+++ JAVA_Demos/src/ControlStructureStmnts/Loops/Pattern5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Please enter no of Rows:");
+		int n=scn.nextInt();
+		 for(int row=1;row<=n;row++) {
+			 for(int col=1;col<=n ; col++) {
+				 if(row==1 || row==n || col==1 || col==n) {
+					 System.out.print("* ");
+				 }else {
+					 System.out.print("  ");
+				 }
+				 
+			 }
+			 System.out.println();
+		 }
+
+
+	}
+
+}
