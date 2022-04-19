ae115d62b5c33dd7266842f8cfc5a7e65852f34c
mshaveta <shaveta.malhotra47@gmail.com>
2022-04-19 09:25:53 +0530

    Added

---
+++ JAVA_Demos/src/OOPs_Concepts/inheritance/ConstOverloading/ChildClass.java
@@ -0,0 +1,12 @@
+package OOPs_Concepts.inheritance.ConstOverloading;
+
+public class ChildClass extends ParentClass {
+	 public ChildClass() {
+		 super(10,100.15);
+	 }
+
+	public static void main(String[] args) {
+		ChildClass obj = new ChildClass();
+		 
+	}
+}
---
+++ JAVA_Demos/src/OOPs_Concepts/inheritance/ConstOverloading/ParentClass.java
@@ -0,0 +1,26 @@
+package OOPs_Concepts.inheritance.ConstOverloading;
+
+public class ParentClass {
+	/* different no. of params*/
+	public ParentClass() {
+		System.out.println("Parent class Constructor with Zero params");
+	}
+	
+	/* different type of params*/
+	public ParentClass(int z) {
+		System.out.println("Parent class Constructor with one int type param");
+	}
+	
+	public ParentClass(double z) {
+		System.out.println("Parent class Constructor with one double type param");
+	}
+	
+	/* different sequence of params*/
+	public ParentClass(int a, double z) {
+		System.out.println("Parent class Constructor with one int and one double type param");
+	}
+	
+	public ParentClass(double z, int a) {
+		System.out.println("Parent class Constructor with one double and one int type param");
+	}
+}
