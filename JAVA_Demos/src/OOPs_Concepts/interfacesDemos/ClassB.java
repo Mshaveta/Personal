eb9e29db8d7669df4d244b1da5f1cadb36f76d17
mshaveta <shaveta.malhotra47@gmail.com>
2022-05-08 20:19:40 +0530

    Added

---
+++ JAVA_Demos/src/OOPs_Concepts/interfacesDemos/ClassB.java
@@ -0,0 +1,22 @@
+package OOPs_Concepts.interfacesDemos;
+
+public class ClassB implements I1 {
+
+	public static void main(String[] args) {
+		 
+
+	}
+
+	@Override
+	public void m1() {
+		 System.out.println();
+		
+	}
+
+	@Override
+	public void m2() {
+		 
+		
+	}
+
+}
---
+++ JAVA_Demos/src/OOPs_Concepts/interfacesDemos/I1.java
@@ -0,0 +1,9 @@
+package OOPs_Concepts.interfacesDemos;
+
+//1.7 features
+public interface I1 {
+	int a = 100; //bydefault public static final
+	public void m1(); //abstract methods
+	public void m2();
+
+}
